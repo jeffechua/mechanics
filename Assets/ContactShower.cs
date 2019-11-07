acc950a3b82ad96e5315184131df66ee3ca3c7a4
Jefferson <jeffe.chua@gmail.com>
2019-11-07 01:00:25 +0000

    Initial commit.

---
+++ Assets/ContactShower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactShower : MonoBehaviour
+{
+
+	public GameObject particlesTemplate;
+	ParticleSystem particles;
+	ParticleSystem.EmitParams ep;
+
+	// Start is called before the first frame update
+	void Start() {
+		particles = Instantiate(particlesTemplate, transform.position, transform.rotation, transform).GetComponent<ParticleSystem>();
+		ep = new ParticleSystem.EmitParams();
+		ep.startColor = Color.red;
+		ep.startSize = 0.2f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		List<ContactPoint2D> contacts = new List<ContactPoint2D>();
+		GetComponent<Collider2D>().GetContacts(contacts);
+		particles.Clear();
+		foreach (ContactPoint2D contact in contacts) {
+			ep.position = contact.point;
+			ep.startSize = contact.normalImpulse;
+			particles.Emit(ep, 1);
+		}
+    }
+}
